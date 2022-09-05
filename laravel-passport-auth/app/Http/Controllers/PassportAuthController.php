7296696a9f40aff05c7c49c5d22aa192b7e3fe53
VanessaAmandoron <20170011433@my.xu.edu.ph>
2022-09-05 10:42:58 +0800

    register successful.

--- laravel-passport-auth/app/Http/Controllers/PassportAuthController.php
+++ laravel-passport-auth/app/Http/Controllers/PassportAuthController.php
@@ -28,4 +28,18 @@ class PassportAuthController extends Controller
  
         return response()->json(['token' => $token], 200);
     }
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+ 
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
 }
--- laravel-passport-auth/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel-passport-auth/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('full_name');
+            $table->integer('p_number');
+            $table->string('user_type');
             $table->rememberToken();
             $table->timestamps();
         });
