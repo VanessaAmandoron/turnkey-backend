f497be9af53e8d932ef187389906279400019de0
carljobs <carljarabelo@gmail.com>
2022-10-12 08:36:39 +0800

    -latest-10/12

--- laravel-passport-auth/routes/api.php
+++ laravel-passport-auth/routes/api.php
@@ -30,8 +30,8 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::put('profile/edit', [AuthController::class, 'EditProfile']);
     Route::get('users', [AuthController::class, 'index']);
     Route::post('verify', [AuthController::class, 'VerifyEmail']);
-      
     
+
     Route::get('property/restore/{id}', [PropertyController::class, 'restore']);//admin
 
     //Route::get('search-property/{title}', [PropertyController::class, 'SearchProperty']); //deleted
@@ -42,20 +42,22 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::get('users-role/agent', [AuthController::class, 'viewUsersRoleAgent']);
     Route::get('users-role/client', [AuthController::class, 'viewUsersRoleClient']);
     Route::get('property-list', [PropertyController::class, 'AgentHasProperty']);
-    Route::get('property', [PropertyController::class, 'index']);
     Route::get('property/{id}', [PropertyController::class, 'show']);
-
+    Route::get('agent/contacts', [SendContactDetailsController::class, 'index']);
     Route::post('property/contact/{id}', [SendContactDetailsController::class, 'create']);//new
+    
+    Route::get('property', [PropertyController::class, 'index']);
+    Route::post('property-create', [PropertyController::class, 'store']);
+    Route::put('property-edit', [PropertyController::class, 'update']);
+    Route::delete('property-delete', [PropertyController::class, 'destroy']);//agent, admin
+    
 });
 
 
 //Agent Route
-Route::group(['middleware' => ['auth:api', 'role:agent']], function(){ //'role:agent|admin'
-    Route::post('property-create', [PropertyController::class, 'store']);
-    Route::put('property-edit', [PropertyController::class, 'update']);
-    Route::delete('property-delete', [PropertyController::class, 'destroy']);//agent, admin
-
-});
+// Route::group(['middleware' => ['auth:api', 'role:agent']], function(){ //'role:agent|admin'
+//trial
+// });
 Route::post('image', [ImageController::class, 'imageStore']);
 
 Route::post('forgot-password', [NewPasswordController::class, 'forgotPassword']);
