560830bfbb386d42cc401ccc079c41d52010e797
carljobs <carljarabelo@gmail.com>
2022-10-12 09:48:54 +0800

    -send-contact-details-latest

--- laravel-passport-auth/app/Http/Controllers/Api/SendContactDetailsController.php
+++ laravel-passport-auth/app/Http/Controllers/Api/SendContactDetailsController.php
@@ -37,6 +37,7 @@ class SendContactDetailsController extends Controller
         $data->client_id = Auth::user()->id; //client_id
         $data->agent_id = Property::find($id)->user_id; //agent_id
         $data->property_id = Property::find($id)->id; //property_id
+        $data->property_title = Property::find($id)->title; //property_id
         $data->first_name = Auth::user()->first_name;
         $data->last_name = Auth::user()->last_name;
         $data->email = Auth::user()->email;
